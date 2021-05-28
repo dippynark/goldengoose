88a7d4f41bc38813d22f0c1b702945e8fa88d027
Luke Addison <lukeaddison785@gmail.com>
2021-05-28 11:14:20 +0100

    Simplify

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,17 @@
-FROM scratch
-EXPOSE 8000
+FROM golang:1.16 as builder
+
+WORKDIR /workspace
+COPY go.mod go.mod
+COPY go.sum go.sum
+RUN go mod download
+
+COPY main.go main.go
+
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o goldengoose main.go
+
+FROM gcr.io/distroless/static:nonroot
+WORKDIR /
+COPY --from=builder /workspace/goldengoose .
+USER nonroot:nonroot
+
 ENTRYPOINT ["/goldengoose"]
-COPY ./bin/ /
--- main.go
+++ main.go
@@ -5,42 +5,14 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"strconv"
+	"sync"
 	"time"
 
-	"github.com/felixge/httpsnoop"
 	"github.com/julienschmidt/httprouter"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type request struct {
-	URL     string      `json:"url"`
-	Method  string      `json:"method"`
-	Headers http.Header `json:"headers"`
-	Body    []byte      `json:"body"`
-}
-
-func init() {
-	prometheus.MustRegister(requestDuration)
-}
-
-var requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-	Name:    "request_duration_seconds",
-	Help:    "Time serving HTTP request",
-	Buckets: prometheus.DefBuckets,
-}, []string{"method", "route", "status_code"})
-
-func promMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		m := httpsnoop.CaptureMetrics(h, w, r)
-		requestDuration.WithLabelValues(r.Method, r.URL.Path, strconv.Itoa(m.Code)).Observe(m.Duration.Seconds())
-	})
-}
-
 const (
-	loopCount      = 0 // 50000000
-	delayWorkCount = 50000000
+	loopCount = 50000000
 )
 
 func doWork() {
@@ -64,26 +36,6 @@ func handler(logger *log.Logger) http.Handler {
 
 		doWork()
 
-		/*var err error
-		rr := &request{}
-		rr.Method = r.Method
-		rr.Headers = r.Header
-		rr.URL = r.URL.String()
-		rr.Body, err = ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		rrb, err := json.Marshal(rr)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		rw.Header().Set("Content-Type", "text/plain")
-		rw.Write(rrb)*/
-
 		rw.Header().Set("Content-Type", "text/plain")
 		rw.Write([]byte("Hello from goldengoose!"))
 
@@ -95,19 +47,21 @@ func delayHandler(logger *log.Logger) http.Handler {
 
 		logClientIP(logger, r)
 
-		done := make(chan int)
+		var wg sync.WaitGroup
 
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			for {
 				select {
-				case <-done:
+				case <-time.After(10 * time.Second):
+					return
+				// Cause select loop to spin
 				default:
 				}
 			}
 		}()
-
-		time.Sleep(10 * time.Second)
-		close(done)
+		wg.Wait()
 
 	})
 }
@@ -135,7 +89,4 @@ func main() {
 	r := httprouter.New()
 	r.Handler("GET", "/", handler(logger))
 	r.Handler("GET", "/delay", delayHandler(logger))
-	r.Handler("GET", "/metrics", promhttp.Handler())
-
-	http.ListenAndServe(":8000", promMiddleware(r))
 }
